edb3ffbb959d04c91c0d529b8847f4ac2f542b84
IfzaRasool <ifzarasoolarain@gmail.com>
2022-05-01 17:46:17 +0300

    Edit todos item

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -83,6 +83,7 @@ class TodoContainer extends React.Component {
                 todos={this.state.todos}
                 handleChangeProps={this.handleChange}
                 deleteTodoProps={this.delTodo}
+                setUpdate={this.setUpdate}
               />
             </div>
           </div>
